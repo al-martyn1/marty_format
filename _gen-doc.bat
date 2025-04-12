f65c5bd1e5755f445584f644935757f3eaffd9e9
al-martyn1 <amart@mail.ru>
2025-04-12 21:47:54 +0300

    Пробуем новые фичи md-pp - arg-list

--- _gen-doc.bat
+++ _gen-doc.bat
@@ -11,6 +11,7 @@
 @set EXAMPLES_INCLUDE="--add-examples-path=%~dp0\." "--add-examples-path=%~dp0\md_"
 @rem "--add-examples-path=%~dp0" "--add-examples-path=%~dp0\md_"
 
+@set ARGLIST_OPTIONS=--arg-list-title=:Значение,:Описание --arg-list-type=table --arg-list-value-style=bold
 
 @set EXCLUDE_DIRS=--batch-exclude-dir=_libs,libs,_lib,lib,tests,test,rc,_generators,conf,_distr_conf,doxy,src,_src,.msvc2019,boost,icons
 @set COMMON=--overwrite --copy-images
@@ -19,7 +20,7 @@
 @rem "--graphviz-output-path=%~dp0\..\doc\.generated_images"
 @rem
 @rem set TEMP_OPTS=--graphviz-keep-temp-dot-files
-@set OPTIONS= %TEMP_OPTS% %COMMON% %VARS% %EXCLUDE_DIRS% %GRAPHVIZ_OPTIONS% %UMBA_MDPP_EXTRA_OPTIONS% %EXAMPLES_INCLUDE% "--batch-scan-recurse=%~dp0\."
+@set OPTIONS= %TEMP_OPTS% %COMMON% %VARS% %EXCLUDE_DIRS% %GRAPHVIZ_OPTIONS% %ARGLIST_OPTIONS% %UMBA_MDPP_EXTRA_OPTIONS% %EXAMPLES_INCLUDE% "--batch-scan-recurse=%~dp0\."
 umba-md-pp %OPTIONS%
 
 @umba-md-pp --help > umba-md-pp-help.txt 2>&1
